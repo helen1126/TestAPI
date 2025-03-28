433f503a1eb04461e010dda38e5bf4f5f26f53b7
helen1126 <lxuanjun8@gmail.com>
2025-03-28 19:26:31 +0800

    fix upadate api file

--- test_api.py
+++ test_api.py
@@ -3,6 +3,7 @@ import json
 import time
 import logging
 
+# 配置日志
 logging.basicConfig(
     filename="test_api.log",
     level=logging.INFO,
@@ -10,6 +11,7 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
+# API地址
 API_URL = "http://127.0.0.1:5000/process"
 
 
@@ -17,21 +19,26 @@ def test_api():
     '''
         测试接口
     '''
+    # 读取测试数据
     with open("examples.json", "r", encoding="gbk") as f:
         example_data = json.load(f)
+
     for example in example_data['examples']:
+        # 获取测试信息
         info = example['info']
         data = example['input_data']
         start_time = time.time()
         response = requests.post(API_URL, json=data)
         end_time = time.time()
         letancy = (end_time - start_time) * 1000
+
         try:
             assert response.status_code == 200
             assert len(response.json().get("data", {}).get("final_weights", [])) == 2
             result = "Pass"
         except AssertionError:
             result = "Fail"
+
         logging.info(
             f"Test: {info}, Result: {result}, Letancy: {letancy}ms, Output: {response.text}"
         )
@@ -46,6 +53,7 @@ def test_wrong_json():
     response = requests.post(API_URL, json=example)
     end_time = time.time()
     letancy = (end_time - start_time) * 1000
+    
     try:
         assert response.status_code == 200
         assert len(response.json().get("data", {}).get("final_weights", [])) == 2
